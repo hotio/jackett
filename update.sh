f2af5825acaa5783584b1e407428a5f0654ce2b8
hotio <26902309+hotio@users.noreply.github.com>
2020-10-05 09:53:49 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -31,7 +31,6 @@ elif [[ ${1} == "screenshot" ]]; then
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jackett/jackett/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    echo "VERSION=${version}" > VERSION
-    echo '{"version":"'"${version}"'"}' > VERSION.json
+    echo '{"version":"'"${version}"'"}' | jq . > VERSION.json
     echo "##[set-output name=version;]${version}"
 fi
