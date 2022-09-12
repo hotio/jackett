941a8f22cc7ceb5a7fa5143dc0cd8b18cd6662a1
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-12 12:29:41 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -33,5 +33,6 @@ else
     old_version=$(jq -r '.version' < VERSION.json)
     changelog=$(jq -r '.changelog' < VERSION.json)
     [[ "${old_version}" != "${version}" ]] && changelog="https://github.com/jackett/jackett/compare/v${old_version}...v${version}"
-    echo '{"version":"'"${version}"'","changelog":"'"${changelog}"'"}' | jq . > VERSION.json
+    version_json=$(cat ./VERSION.json)
+    jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
 fi
