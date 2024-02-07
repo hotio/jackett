ec4a1f96b92b6019099604b20c9b1af0e9e4e308
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 10:47:46 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jackett/jackett/releases" | jq -re .[0].tag_name)
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jackett/jackett/releases" | jq -re '.[0].tag_name')
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
