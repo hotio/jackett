0c0be61d9a6ad5b275d631ac937250bbfa444003
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 08:17:28 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jackett/jackett/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${version} ]] && exit 0
-old_version=$(jq -r '.version' < VERSION.json)
-changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/jackett/jackett/compare/v${old_version}...v${version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
+set -e
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jackett/jackett/releases/latest" | jq -re .tag_name)
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    '.version = $version' <<< "${json}" | tee VERSION.json
