a1adc0ecf1217f33c0e4e34f4e39329038b9cfc0
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 16:27:58 +0200

    add __command [skip ci]

--- update-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-version=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/jackett/jackett/releases" | jq -re '.[0].tag_name')
-json=$(cat meta.json)
-jq --sort-keys \
-    --arg version "${version//v/}" \
-    '.version = $version' <<< "${json}" | tee meta.json
